12731b9a9dfe29740ee47bd00d9961b527c6c0e1
viktorbeznosov <viktorbeznosov@mail.ru>
2023-11-13 20:09:21 +0300

    2.2. Модель данных - Post. [#285211 #459416]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -67,12 +67,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && title.equals(post.title) && link.equals(post.link);
+        return id == post.id && link.equals(post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link);
+        return Objects.hash(id, link);
     }
 
     @Override
